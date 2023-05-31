dcc230ded006736e71bebeb7b66ae1a14742f9f9
”fullstackmalina” <malinkin@mail.ru>
2023-05-31 12:58:58 +0300

    Full commit

---
+++ js/index.js
@@ -0,0 +1,138 @@
+const dayWeekList = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dayNumberElements = document.querySelectorAll(".page-nav__day-number");
+    const dayWeekElements = document.querySelectorAll(".page-nav__day-week");
+
+    const today = new Date();
+    today.setHours(0, 0, 0);
+
+    dayNumberElements.forEach((dayNumberElement, item) => {
+        const day = new Date(today.getTime() + (item * 24 * 60 * 60 * 1000));
+        const timestamp = Math.trunc(day / 1000);
+        dayNumberElement.innerHTML = `${day.getDate()},`;
+        dayWeekElements[item].innerHTML = dayWeekList[day.getDay()];
+        const link = dayNumberElement.parentNode;
+        link.dataset.timeStamp = timestamp;
+        if (dayWeekElements[item].innerHTML === 'Вс' || dayWeekElements[item].innerHTML === 'Сб') {
+            link.classList.add('page-nav__day_weekend');
+        } else {
+            link.classList.remove('page-nav__day_weekend');
+        }
+    });
+
+
+    const url = 'https://jscp-diplom.netoserver.ru/';
+    const headers = new Headers({
+        'Content-Type': 'application/x-www-form-urlencoded'
+    });
+    const body = new URLSearchParams({
+        'event': 'update'
+    });
+
+    //запрос данных с сервера
+    fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: body
+    })
+        .then(response => response.json())
+
+        .then(data => {
+            //ответ от сервера = объект
+
+            data.seances = data.seances.result;
+            data.films = data.films.result;
+            data.halls = data.halls.result;
+            data.halls = data.halls.filter((hall) => hall.hall_open == 1);
+
+            const main = document.querySelector("main");
+            data.films.forEach((film) => {
+                let seancesHTML = '';
+                const filmId = film.film_id;
+                data.halls.forEach((hall) => {
+                    const seances = data.seances.filter((seance) => ((seance.seance_hallid == hall.hall_id) && (seance.seance_filmid == filmId)));
+                    if (seances.length > 0) {
+                        seancesHTML += `
+                    <div class="movie-seances__hall">
+                        <h3 class="movie-seances__hall-title">${hall.hall_name}</h3>
+                        <ul class="movie-seances__list">`;
+                        seances.forEach((seance) => {
+                            seancesHTML += `<li class="movie-seances__time-block"><a class="movie-seances__time" href="hall.html" data-film-name="${film.film_name}" data-film-id="${film.film_id}" data-hall-id="${hall.hall_id}" data-hall-name="${hall.hall_name}" data-price-vip="${hall.hall_price_vip}" data-price-standart="${hall.hall_price_standart}" data-seance-id="${seance.seance_id}" data-seance-start="${seance.seance_start}" data-seance-time="${seance.seance_time}">${seance.seance_time}</a></li>`;
+                        });
+                        seancesHTML += `
+                        </ul>
+                    </div>`;
+                    }
+                });
+                if (seancesHTML) {
+                    main.innerHTML += `
+                                <section class="movie">
+                                <div class="movie__info">
+                                    <div class="movie__poster">
+                                    <img class="movie__poster-image" alt="Звёздные войны постер" src="${film.film_poster}">
+                                    </div>
+                                    <div class="movie__description">
+                                    <h2 class="movie__title">${film.film_name}</h2>
+                                    <p class="movie__synopsis">${film.film_description}</p>
+                                    <p class="movie__data">
+                                        <span class="movie__data-duration">${film.film_duration} мин.</span>
+                                        <span class="movie__data-origin">${film.film_origin}</span>
+                                    </p>
+                                    </div>
+                                </div>
+                                ${seancesHTML}
+                                </section>
+                                `
+                }
+            })
+
+            const movieSeances = Array.from(document.querySelectorAll(".movie-seances__time"));
+            const dayLinks = Array.from(document.querySelectorAll(".page-nav__day"));
+
+            const getTimeStampDay = (event) => {
+                let timeStampDay = Number(event.target.dataset.timeStamp);
+                if (isNaN(timeStampDay)) {
+                    timeStampDay = Number(event.target.closest(".page-nav__day").dataset.timeStamp);
+                }
+                return timeStampDay;
+            };
+
+            const updateSeances = (timeStampDay) => {
+                movieSeances.forEach((movieSeance) => {
+                    const timeStampSeanceDay = Number(movieSeance.dataset.seanceStart) * 60;
+                    const timeStampSeance = timeStampDay + timeStampSeanceDay;
+                    const timeStampNow = Math.trunc(+new Date() / 1000);
+                    movieSeance.dataset.seanceTimeStamp = timeStampSeance;
+                    console.log("timeStampDay", timeStampDay);
+                    movieSeance.classList.toggle(
+                        "acceptin-button-disabled",
+                        timeStampSeance - timeStampNow <= 0
+                    );
+                });
+            };
+            //выбор в шапке дня для заказа
+            dayLinks.forEach((dayLink) =>
+                dayLink.addEventListener("click", (event) => {
+                    event.preventDefault();
+                    document.querySelector(".page-nav__day_chosen").classList.remove("page-nav__day_chosen");
+                    dayLink.classList.add("page-nav__day_chosen");
+                    const timeStampDay = getTimeStampDay(event);
+                    updateSeances(timeStampDay);
+                })
+            );
+
+            movieSeances.forEach((movieSeance) =>
+                movieSeance.addEventListener("click", (event) => {
+                    const { hallId } = event.target.dataset;
+                    const selectSeanse = {
+                        ...event.target.dataset,
+                        hallConfig: data.halls.find((hall) => hall.hall_id == hallId).hall_config,
+                    };
+
+                    window.sessionStorage.setItem("selectSeanse", JSON.stringify(selectSeanse));
+                })
+            );
+        })
+        .catch(error => console.error(error));
+})
---
+++ js/ticket.js
@@ -0,0 +1,57 @@
+
+const selectedSeats = JSON.parse(sessionStorage.getItem('selectedSeats')); // получаем объект выбранных мест
+const selectedFilmInfo = JSON.parse(sessionStorage.getItem('selectSeanse'));
+
+const titleFilm = document.querySelector('.ticket__details.ticket__title');
+titleFilm.innerHTML += `${selectedFilmInfo.filmName}`;
+
+let timeStamp = Number(selectedFilmInfo.seanceTimeStamp);
+let date = new Date(timeStamp);
+let dataSeance = date.toLocaleDateString("ru-Ru", {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+});
+
+let totalPrice = 0;
+const seatInfo = document.querySelector('.ticket__details.ticket__chairs');
+for (let seat of selectedSeats) {
+    seatInfo.innerHTML += ` ${seat.row}/${seat.seat} `; // отображаем информацию о месте
+    totalPrice += Number(seat.price); // вычисляем общую стоимость
+};
+const seatInfoForQR = seatInfo.innerHTML;
+
+
+const titleHall = document.querySelector('.ticket__details.ticket__hall');
+const titleHallValue = selectedFilmInfo.hallName.substr(selectedFilmInfo.hallName.length - 1)
+titleHall.innerHTML += `${titleHallValue}`;
+
+const filmStart = document.querySelector('.ticket__details.ticket__start');
+filmStart.innerHTML += `${selectedFilmInfo.seanceTime}`;
+
+
+const totalPriceInfo = document.querySelector('.ticket__details.ticket__prise');
+totalPriceInfo.innerHTML += `${totalPrice}руб.`; // отображаем общую стоимость
+
+
+const QRgenetator = `
+Фильм: ${selectedFilmInfo.filmName}
+Зал: ${titleHallValue}
+Ряд/Место: ${seatInfoForQR}
+Дата: ${dataSeance}
+Начало сеанса: ${selectedFilmInfo.seanceTime}
+
+Билет действителен строго на свой сеанс!
+`;
+
+const qrcode = QRCreator(QRgenetator);
+
+// const someFunctWithSelect = () => {
+const content = (qrcode) => {
+    return qrcode.error
+        ? `недопустимые исходные данные ${qrcode.error}`
+        : qrcode.result;
+};
+
+document.getElementById("qrcode").append("", content(qrcode));
+
